51c1b3287681346a9a2f5ed53976920ee57ea9ea
JeanFuentesCode <jeanmrcode@gmail.com>
2026-08-29 13:02:35 -0400

    Add POST endpoint for PDF preview

--- src/server.js
+++ src/server.js
@@ -44,4 +44,7 @@ app.use((err, req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
+
+app.post('/api/preview', upload.array('pdfs', 1), previewController.preview);
+  
 });
